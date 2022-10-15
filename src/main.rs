a8cd92053d5fbbfa5bee271760507cdde9284181
Dominic Hamon <dma+github@stripysock.com>
2022-10-15 14:35:01 +0100

    better error logging

--- src/main.rs
+++ src/main.rs
@@ -27,21 +27,26 @@ enum Commands {
     Tag {},
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
+    env_logger::init();
     let args = Args::parse();
     match &args.command {
         Commands::Add { path } => {
-            println!("adding {:?}", path);
+            log::info!("adding {:?}", path);
         }
         Commands::Init { path } => {
-            println!("initializing grist repo at {:?}", path);
-            let cwd = std::env::current_dir()?;
+            log::info!("initializing grist repo at {:?}", path);
+            let cwd_or_err = std::env::current_dir();
+            if cwd_or_err.is_err() {
+                panic!("failed to get current directory");
+            }
+            let cwd = cwd_or_err.unwrap();
             let repo_path = path.as_ref().unwrap_or(&cwd);
-            Repo::create(repo_path)?;
+            match Repo::create(repo_path) {
+                Ok(_) => println!("created repo at {:?}", repo_path),
+                Err(error) => log::error!("failed to create repo at {:?}: {}", repo_path, error),
+            }
         }
-        _ => println!("oops"),
+        _ => panic!("command"),
     }
-    println!("Hello, world!");
-
-    Ok(())
 }
