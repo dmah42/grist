f53afea551eba7cb20d0ee78b98b03bfdc38e511
Dominic Hamon <dma+github@stripysock.com>
2022-10-15 15:13:43 +0100

    factor out command methods

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate ini;
 
 use crate::repo::Repo;
 use clap::{Parser, Subcommand};
+use std::{error::Error, path::PathBuf};
 
 #[derive(Parser)]
 #[command(author, version, about)]
@@ -19,7 +20,7 @@ enum Commands {
     Add { path: std::path::PathBuf },
     Checkout {},
     Commit {},
-    Init { path: Option<std::path::PathBuf> },
+    Init { path: Option<PathBuf> },
     Log {},
     Merge {},
     Rebase {},
@@ -27,26 +28,34 @@ enum Commands {
     Tag {},
 }
 
+fn add(path: &PathBuf) -> Result<(), Box<dyn Error>> {
+    log::info!("adding {:?}", path);
+    Ok(())
+}
+
+fn init(path: &Option<PathBuf>) -> Result<Repo, Box<dyn Error>> {
+    log::info!("initializing grist repo at {:?}", path);
+    let cwd_or_err = std::env::current_dir();
+    if cwd_or_err.is_err() {
+        panic!("failed to get current directory");
+    }
+    let cwd = cwd_or_err.unwrap();
+    let repo_path = path.as_ref().unwrap_or(&cwd);
+    Repo::create(repo_path)
+}
+
 fn main() {
     env_logger::init();
     let args = Args::parse();
     match &args.command {
-        Commands::Add { path } => {
-            log::info!("adding {:?}", path);
-        }
-        Commands::Init { path } => {
-            log::info!("initializing grist repo at {:?}", path);
-            let cwd_or_err = std::env::current_dir();
-            if cwd_or_err.is_err() {
-                panic!("failed to get current directory");
-            }
-            let cwd = cwd_or_err.unwrap();
-            let repo_path = path.as_ref().unwrap_or(&cwd);
-            match Repo::create(repo_path) {
-                Ok(_) => println!("created repo at {:?}", repo_path),
-                Err(error) => log::error!("failed to create repo at {:?}: {}", repo_path, error),
-            }
-        }
+        Commands::Add { path } => match add(path) {
+            Ok(_) => println!("added {:?}", path),
+            Err(error) => log::error!("failed to add {:?}: {}", path, error),
+        },
+        Commands::Init { path } => match init(path) {
+            Ok(_) => println!("initialized repo at {:?}", path),
+            Err(error) => log::error!("failed to initialize repo at {:?}: {}", path, error),
+        },
         _ => panic!("command"),
     }
 }
